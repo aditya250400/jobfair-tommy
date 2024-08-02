24f7e28e81901ded7db09f1542813deee792819f
aditya250400 <muhamadrizkiaditya32@gmail.com>
2024-08-03 00:02:56 +0700

    second commit

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,9 @@
 import { Server } from "./server";
 import prisma from '../prisma/db'
-import listEndpoints from 'express-list-endpoints';
+
 
 (async () => {
     await prisma.$connect()
     const server = new Server()
     await server.start()
-
-     const app = server.app;
-
-    // Log routes to the console
-    const routes = listEndpoints(app);
-    console.log(routes);
 })()
